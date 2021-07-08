e92ac402ad2fbc453a12ebebeb9b6e1132121d11
neha <siddireddyldbhavani431@gmail.com>
2021-07-08 20:31:23 +0530

    test

--- sketch.js
+++ sketch.js
@@ -9,9 +9,10 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
+var score=0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -42,10 +43,17 @@ function setup(){
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
+
+   
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg)
+        background(backgroundImg);
+        textSize(35);
+        fill ("white");
+        text("Score "+ score ,width-300,50);
+   
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -85,4 +93,21 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImg(){
+    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    var responseJSON=await response.json();
+    var datetime=responseJSON.datetime;
+    var hour=datetime.slice(11,13);
+    console.log(hour);
+
+    if(hour>=06&&hour<=19){
+        bg ="sprites/bg.png";
+
+    }
+    else{
+        bg="sprites/bg2.jpg";
+    }
+    backgroundImg=loadImage(bg);
 }
